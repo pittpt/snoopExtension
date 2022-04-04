560ef847aacab38241f0c2948b6d58e338bba267
pittpt <97823090+pittpt@users.noreply.github.com>
2022-04-04 21:42:40 +0700

    reorganized

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
-const routes = require("./routes/routes");
+const routes = require("./database/routes/routes");
 
 const mongoString = process.env.DATABASE_URL;
 const port = process.env.PORT || 3000;
